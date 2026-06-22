ee83240699105912a7a93ce70cf9861d4d2d515b
sobcombo <zoomzoomzoomer00@gmail.com>
2026-06-23 00:01:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbz4JlgvFAPwvjXq_NMZ6Qqrz47RppmnQndXEe8FFrWb1HfFLWWPT0VHXbQF7tJ79Qr7/exec";
 
-loadDashboard();
+async function loadDashboard() {
+  try {
 
+    const response = await fetch(
+      API_URL + "?action=dashboard"
+    );
 
+    const data = await response.json();
 
-async function loadDashboard(){
-
-  try{
-
-    const response =
-      await fetch(
-        API_URL + "?action=dashboard"
-      );
-
-    const data =
-      await response.json();
-
-    document.getElementById(
-      "totalSales"
-    ).innerText =
+    document.getElementById("totalSales").innerText =
       "₹ " + data.totalSales;
 
-    document.getElementById(
-      "totalPurchase"
-    ).innerText =
+    document.getElementById("totalPurchase").innerText =
       "₹ " + data.totalPurchase;
 
-    document.getElementById(
-      "pendingPayments"
-    ).innerText =
+    document.getElementById("pendingPayments").innerText =
       "₹ " + data.pendingPayments;
 
-  }
-  catch(err){
-
+  } catch (err) {
     console.error(err);
-
   }
 }
 
